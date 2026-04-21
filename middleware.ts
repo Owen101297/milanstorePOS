e10ec8af2cfa2056dd3585d4e8393b8a28973f3f
Owen <tu_correo@ejemplo.com>
2026-04-20 23:47:27 -0500

    fix: NextResponse import in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-﻿import { type NextRequest } from 'next/server'
+﻿import { type NextRequest, NextResponse } from 'next/server'
 import { createServerClient } from '@supabase/ssr'
 
 export async function middleware(request: NextRequest) {
